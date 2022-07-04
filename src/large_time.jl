27125d515945d89b45b1727035fc5b2e50944ca4
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-04 10:51:27 +1000

    new Psi methods

--- src/large_time.jl
+++ src/large_time.jl
@@ -1,43 +1,69 @@
 """
-viscous_solution_large_time(x, μ::Float64, t::Float64)
-viscous_solution_large_time(x::Vector{T}, μ::Vector{Float64}, t::Vector{Float64}) where {T<:Number}
-viscous_solution_large_time(x::Vector{T}, y::Vector{T}, μ::Vector{Float64}, t::Vector{Float64}) where {T<:Number}
-viscous_solution_large_time_Ψ(x::Vector{T}, y::Vector{T}, μ::Vector{Float64}) where {T<:Number}
-
-Function for computing the large-time limit of the exact solution to the viscous problem. The last method computes 
-`Ψ` rather than `u`.
+    viscous_solution_large_time_Ψ(η, μ)
+    viscous_solution_large_time_Ψ(x::AbstractVector, y::AbstractVector, μ::AbstractVector)
+    viscous_solution_large_time_Ψ(x::AbstractVector, μ::AbstractVector)
+    viscous_solution_large_time_Ψ(x::AbstractVector{T}, y::AbstractVector{T}) where {T<:Number}
+    viscous_solution_large_time(x, t::Float64, μ::Float64k, t₀ = 0.0)
+    viscous_solution_large_time(x::AbstractVector{T}, t::AbstractVector{Float64}, μ::AbstractVector{Float64}, t₀ = 0.0) where {T<:Number}
+    viscous_solution_large_time(x::AbstractVector{T}, y::AbstractVector{T}, t::AbstractVector{Float64}, μ::AbstractVector{Float64}, t₀ = 0.0) where {T<:Number}
+    viscous_solution_large_time(x::AbstractVector{T}, t, μ::AbstractVector{Float64}, t₀ = 0.0) where {T<:Number}
+    
+Function for computing the large-time limit of the exact solution to the viscous problem.
 """
-function viscous_solution_large_time end 
-function viscous_solution_large_time(x, t::Float64, μ::Float64)
-    return 2sqrt(μ / (π * t)) * exp(-x^2 / (4 * μ * t)) / (coth(π / (4μ)) - erf(x / (2sqrt(μ * t))))
+function viscous_solution_large_time end
+@doc (@doc viscous_solution_large_time) function viscous_solution_large_time_Ψ(η, μ)
+    2 / sqrt(π) * exp(-η^2 / 4) / (coth(π / (4μ)) - erf(η / 2))
 end
-function viscous_solution_large_time(x::Vector{T}, t::Vector{Float64}, μ::Vector{Float64}) where {T<:Number}
+@doc (@doc viscous_solution_large_time) function viscous_solution_large_time_Ψ(x::AbstractVector, y::AbstractVector, μ::AbstractVector)
+    sol = zeros(ComplexF64, length(x), length(y), length(μ))
+    for (k, m) in enumerate(μ)
+        for (j, yy) in enumerate(y)
+            for (i, xx) in enumerate(x)
+                sol[i, j, k] = viscous_solution_large_time_Ψ(complex(xx, yy), m)
+            end
+        end
+    end
+    sol
+end
+@doc (@doc viscous_solution_large_time) function viscous_solution_large_time_Ψ(x::AbstractVector, μ::AbstractVector)
+    sol = zeros(Float64, length(x), length(μ))
+    for (j, m) in enumerate(μ)
+        for (i, xx) in enumerate(x)
+            sol[i, j] = viscous_solution_large_time_Ψ(xx, m)
+        end
+    end
+    sol
+end
+function viscous_solution_large_time(x, t::Float64, μ::Float64, t₀ = 0.0)
+    η = x/sqrt(μ*(t-t₀))
+    Ψ = viscous_solution_large_time_Ψ(η, μ)
+    return sqrt(μ/(t-t₀)) * Ψ
+end
+function viscous_solution_large_time(x::AbstractVector{T}, t::AbstractVector{Float64}, μ::AbstractVector{Float64}, t₀ = 0.0) where {T<:Number}
     vals = zeros(T, length(x), length(t), length(μ))
     for (k, μμ) in enumerate(μ)
         for (j, tt) in enumerate(t)
             for (i, xx) in enumerate(x)
-                vals[i, j, k] = viscous_solution_large_time(xx, tt, μμ)
+                vals[i, j, k] = viscous_solution_large_time(xx, tt, μμ, t₀)
             end
         end
     end
     return vals
 end
-function viscous_solution_large_time(x::Vector{T}, y::Vector{T}, t::Vector{Float64}, μ::Vector{Float64}) where {T<:Number}
+function viscous_solution_large_time(x::AbstractVector{T}, t, μ::AbstractVector{Float64}, t₀ = 0.0) where {T<:Number}
+    viscous_solution_large_time(x, [t], μ, t₀)[:, 1, :]
+end
+function viscous_solution_large_time(x::AbstractVector{T}, y::AbstractVector{T}, t::AbstractVector{Float64}, μ::AbstractVector{Float64}, t₀ = 0.0) where {T<:Number}
     vals = zeros(ComplexF64, length(x), length(y), length(t), length(μ))
     for (ℓ, μμ) in enumerate(μ)
         for (k, tt) in enumerate(t)
             for (j, yy) in enumerate(y)
                 for (i, xx) in enumerate(x)
                     z = xx + im * yy
-                    vals[i, j, k, ℓ] = viscous_solution_large_time(z, tt, μμ)
+                    vals[i, j, k, ℓ] = viscous_solution_large_time(z, tt, μμ, t₀)
                 end
             end
         end
     end
-    return vals
-end
-@doc (@doc viscous_solution_large_time) function viscous_solution_large_time_Ψ(x::Vector{T}, y::Vector{T}, μ::Vector{Float64}) where {T<:Number}
-    vals = viscous_solution_large_time(x, y, μ, [1.0])
-    vals = vals[:, :, 1, :]
     return vals
 end
