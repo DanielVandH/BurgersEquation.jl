74392320c95588a69c644cc0d21390eea2f8e2f9
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-19 13:29:59 +1000

    only plot aaa in upper

--- paper/aaa.jl
+++ paper/aaa.jl
@@ -4,7 +4,7 @@ N = 1000
 t = [0.1, 0.5, 1.0, 8sqrt(3) / 9]
 μ = 0.1
 X = -4:0.01:4
-Y = -4:0.01:4
+Y = 0:0.01:4
 U = viscous_solution_aaa(N, L, T, t, μ, X, Y)
 
 time_labels = ["0.1", "0.5", "1", "t_s"]
@@ -15,13 +15,14 @@ for j in 1:length(t)
     portrait!(fig, X, Y, U[:, :, j], plot_layout[j][1][1], plot_layout[j][1][2]; nist=NIST,
         title=L"(%$(alphabets[j][1])): $t = %$(time_labels[j])$", titlealign=:left,
         xticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]),
-        yticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]))
+        yticks=([0.0, 2.0, 4.0], [L"0", L"2", L"4"]))
     landscape!(fig, X, Y, U[:, :, j], plot_layout[j][2][1], plot_layout[j][2][2]; nist=NIST,
-        title=L"(%$(alphabets[j][2])): $t = %$(time_labels[j])$", titlealign=:left, zlims=(0, 10),
+        title=L"(%$(alphabets[j][2])): $t = %$(time_labels[j])$", titlealign=:left, zlims=(0, 5),
         xticks=([-4.0, 0.0, 4.0], [L"-4", L"0", L"4"]),
-        yticks=([-4.0, 0.0, 4.0], [L"-4", L"0", L"4"]),
-        zticks=([0.0, 5.0, 10.0], [L"0", L"5", L"10"]),
+        yticks=([0.0, 2.0, 4.0], [L"0", L"2", L"4"]),
+        zticks=([0.0, 2.5, 5.0], [L"0", L"2.5", L"5"]),
         zlabel=L"|r(z, t)|")
+    lines!(fig.content[end], X, zeros(length(X)), U[:, 1, j], color=:black, linewidth=5)
 end
 resize_to_layout!(fig)
 save("$FIGURES/AAA_viscous_solutions.$EXTENSION", fig)
