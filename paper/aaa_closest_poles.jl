80506b5a9ba4c50dcc27be3d244c76ef28acd20c
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-19 13:47:17 +1000

    aaa labels, upper

--- paper/aaa_closest_poles.jl
+++ paper/aaa_closest_poles.jl
@@ -4,7 +4,7 @@ N = 1000
 t = [0.1, 0.5, 1.0, 8sqrt(3) / 9]
 μ = 0.1
 X = -4:0.01:4
-Y = -4:0.01:4
+Y = 0:0.01:4
 U = viscous_solution_aaa(N, L, T, t, μ, X, Y)
 t = 1.0
 μ = 0.1
@@ -14,20 +14,20 @@ fig = Figure(fontsize=25)
 portrait!(fig, X, Y, U[:, :, 3], 1, 1; nist=NIST, width=400, height=400,
     title=L"(a):$ $ AAA approximant", titlealign=:left,
     xticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]),
-    yticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]))
+    yticks=([ 0.0, 2.0, 4.0], [L"0", L"2", L"4"]))
 portrait!(fig, X, Y, u, 1, 2; nist=NIST,
     title=L"(b):$ $ Exact solution", titlealign=:left, width=400, height=400,
     xticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]),
-    yticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]))
+    yticks=([.0, 2.0, 4.0], [L"0", L"2", L"4"]))
 
-text!(fig.content[1], L"r_1", position=(1.0, 0.4), color=:white, textsize=36)
-text!(fig.content[1], L"r_2", position=(-2.0, 2.0), color=:black, textsize=36)
-text!(fig.content[1], L"r_3", position=(-2.0, -2.0), color=:white, textsize=36)
-text!(fig.content[1], L"r_4", position=(1.0, -0.6), color=:white, textsize=36)
-text!(fig.content[2], L"z_1", position=(1.0, 0.4), color=:white, textsize=36)
-text!(fig.content[2], L"z_2", position=(-2.0, 2.0), color=:black, textsize=36)
-text!(fig.content[2], L"z_3", position=(-2.0, -2.0), color=:white, textsize=36)
-text!(fig.content[2], L"z_4", position=(1.0, -0.6), color=:white, textsize=36)
+text!(fig.content[1], L"r_0^{(1)}", position=(0.7, 0.4), color=:white, textsize=36)
+text!(fig.content[1], L"r_0^{(2)}", position=(-2.3, 2.0), color=:black, textsize=36)
+#text!(fig.content[1], L"r_3", position=(-2.0, -2.0), color=:white, textsize=36)
+#text!(fig.content[1], L"r_4", position=(1.0, -0.6), color=:white, textsize=36)
+text!(fig.content[2], L"z_0^{(1)}", position=(1.0, 0.4), color=:white, textsize=36)
+text!(fig.content[2], L"z_0^{(2)}", position=(-2.0, 1.9), color=:black, textsize=36)
+#text!(fig.content[2], L"z_3", position=(-2.0, -2.0), color=:white, textsize=36)
+#text!(fig.content[2], L"z_4", position=(1.0, -0.6), color=:white, textsize=36)
 
 resize_to_layout!(fig)
 
