167c2a0e26a3f4576288cc8a653ae281c3a0ebb1
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-25 11:35:40 +1000

    Use new constants, remove comments, new matlab name

--- paper/aaa.jl
+++ paper/aaa.jl
@@ -1,4 +1,4 @@
-# Below is one way to make these plots in Julia, but we just use the MATLAB results since they seem to be more accurate. 
+# Below is one way to make these plots in Julia, but we just use the MATLAB results since they seem to be more accurate. See "constants.jl" and "burger_aaa.m".
 #=
 L = 30
 T = 2.0
@@ -10,24 +10,22 @@ Y = 0:0.01:4
 U = viscous_solution_aaa(N, L, T, t, μ, X, Y)
 =#
 
-# The constants below are obtained in the MATLAB script "burger_aaa.m".
-
 time_labels = ["0.1", "0.5", "1", "t_s"]
 plot_layout = [[(1, 1), (1, 2)], [(1, 3), (1, 4)], [(2, 1), (2, 2)], [(2, 3), (2, 4)]]
 alphabets = [['a', 'b'], ['c', 'd'], ['e', 'f'], ['g', 'h']]
 fig = Figure(fontsize=38, resolution=(2000, 800))
 for j in 1:length(t)
-    portrait!(fig, X, Y, U[:, :, j], plot_layout[j][1][1], plot_layout[j][1][2]; nist=NIST,
+    portrait!(fig, X_AAA, Y_AAA, U_AAA[j], plot_layout[j][1][1], plot_layout[j][1][2]; nist=NIST,
         title=L"(%$(alphabets[j][1])): $t = %$(time_labels[j])$", titlealign=:left,
         xticks=([-4.0, -2.0, 0.0, 2.0, 4.0], [L"-4", L"-2", L"0", L"2", L"4"]),
         yticks=([0.0, 2.0, 4.0], [L"0", L"2", L"4"]))
-    landscape!(fig, X, Y, U[:, :, j], plot_layout[j][2][1], plot_layout[j][2][2]; nist=NIST,
+    landscape!(fig, X_AAA, Y_AAA, U_AAA[j], plot_layout[j][2][1], plot_layout[j][2][2]; nist=NIST,
         title=L"(%$(alphabets[j][2])): $t = %$(time_labels[j])$", titlealign=:left, zlims=(0, 5),
         xticks=([-4.0, 0.0, 4.0], [L"-4", L"0", L"4"]),
         yticks=([0.0, 2.0, 4.0], [L"0", L"2", L"4"]),
         zticks=([0.0, 2.5, 5.0], [L"0", L"2.5", L"5"]),
         zlabel=L"|r(z, t)|")
-    lines!(fig.content[end], X, zeros(length(X)), U[:, 1, j], color=:black, linewidth=5)
+    lines!(fig.content[end], X_AAA, zeros(length(X_AAA)), U_AAA[j][:, 1], color=:black, linewidth=5)
 end
 resize_to_layout!(fig)
 save("$FIGURES/AAA_viscous_solutions.$EXTENSION", fig)
