ab723aa388594aa22f6c278edcb9001aebaaafe6
DanielVandH <danj.vandenheuvel@gmail.com>
2022-07-25 13:30:07 +1000

    move colour wheel

--- paper/exact_solution.jl
+++ paper/exact_solution.jl
@@ -38,14 +38,3 @@ for k = 1:length(μ)
     fig
     save("$FIGURES/viscous_solutions_$k.$EXTENSION", fig)
 end
-
-# Colour wheel 
-x = -1:0.001:1
-y = -1:0.001:1
-z = [x + im * y for x in x, y in y]
-z[abs.(z).>1] .= NaN .+ im * NaN
-fig = Figure(fontsize=15, resolution=(300, 300))
-portrait!(fig, x, y, z, 1, 1; nist=NIST,
-    xticks=([-1.0, -0.5, 0.0, 0.5, 1.0], [L"-1", L"-0.5", L"0", L"0.5", L"1"]),
-    yticks=([-1.0, -0.5, 0.0, 0.5, 1.0], [L"-1", L"-0.5", L"0", L"0.5", L"1"]))
-save("$FIGURES/colour_wheel.$EXTENSION", fig)
